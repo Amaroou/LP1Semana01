a09f8dd00449ba4c637bbc266db59855ade1601e
Amaro <Jamaro2110@gmail.com>
2023-03-03 11:24:16 +0000

    TiposVarios first commit

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro1 = 3;
+            int inteiro2 = 5;
+            int inteiro3 = 22;
+            Console.WriteLine(inteiro1);
+            Console.WriteLine(inteiro2);
+            Console.WriteLine(inteiro3);
+        }
+    }
+}
