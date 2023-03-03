eac8962e8d27528d86846d329722b4fc822e1e5e
Amaro <Jamaro2110@gmail.com>
2023-03-03 11:34:35 +0000

    TiposVarios unicode added

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -12,6 +12,12 @@ namespace TiposVarios
             Console.WriteLine(inteiro1);
             Console.WriteLine(inteiro2);
             Console.WriteLine(inteiro3);
+            char degre_sign = '\u00B0';
+            char broken_bar = '\u00A6';
+            Console.WriteLine(degre_sign);
+            Console.WriteLine(broken_bar);
+
+
         }
     }
 }
