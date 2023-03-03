5a24421d647c379d3ce2547c7148cb875add1a40
Amaro <Jamaro2110@gmail.com>
2023-03-03 14:30:10 +0000

    Exercicio 6

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -2,10 +2,13 @@
 
 namespace VariasStrings
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            double xx= 0.12345;
+            int ii = 18;
             string s = "C:\\Users\\jamar\\TRABALHOSLP01\\LP1Semana01";
             string s2 = @"C:\Users\jamar\TRABALHOSLP01\LP1Semana01";
             string d = "\"String original entre aspas\"";
@@ -14,6 +17,8 @@ namespace VariasStrings
             Console.WriteLine(s2);
             Console.WriteLine(d);
             Console.WriteLine(d2);
+            Console.WriteLine("{0:f2} e {0:p1}", xx, xx);
+            Console.WriteLine("{0:x0} e {0:c0}", ii, ii);
         }
     }
 }
