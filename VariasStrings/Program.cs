2ed998d85e8fa1addac2d054b7e8fd590e94f9ab
Amaro <Jamaro2110@gmail.com>
2023-03-03 11:54:18 +0000

    Exercicio 4

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "C:\\Users\\jamar\\TRABALHOSLP01\\LP1Semana01";
+            string s2 = @"C:\Users\jamar\TRABALHOSLP01\LP1Semana01";
+            string d = "\"String original entre aspas\"";
+            string d2 = @"""String ainda mais original entre aspas""";
+            Console.WriteLine(s);
+            Console.WriteLine(s2);
+            Console.WriteLine(d);
+            Console.WriteLine(d2);
+        }
+    }
+}
